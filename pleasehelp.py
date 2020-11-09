3ba583f5c9c4d47dc601896299b7f1fc3ef0b4be
Titus Nguyen <titusstar007@gmail.com>
2020-11-09 16:19:51 -0700

    The status works now
    added #track which is extremely basic without even outputing to discord

--- pleasehelp.py
+++ pleasehelp.py
@@ -1,27 +1,31 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='*')
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
 
+client = commands.Bot(command_prefix='#', intents=intents)
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
+memberList = []
 
 @client.command()
-async def hello(ctx):
-    await ctx.send('Hello!')
+async def track(ctx, member: discord.Member):
+    await ctx.send(member.status)
+    memberList.append(member)
 
-with open('token.txt') as tokenfile:
-    token = tokenfile.readline()
+@client.event
+async def on_member_update(before, after):
+    for member in memberList:
+        if member == after:
+            print(before.status, after.status)
+
+
+with open('token.txt') as tokenFile:
+    token = tokenFile.readline()
     client.run(token)
